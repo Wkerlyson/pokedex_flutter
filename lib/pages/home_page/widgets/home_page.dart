bca950c8c8df6c004a19c0b9c3a227ef2e82bfe4
Wkerlyson <wkerlyson.batista@gmail.com>
2020-02-25 15:02:35 -0300

    first commit

---
+++ lib/pages/home_page/widgets/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_flutter/consts/consts_app.dart';
+import 'package:pokedex_flutter/pages/home_page/widgets/app_bar_home.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double statusWidth = MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        alignment: Alignment.topCenter,
+        overflow: Overflow.visible,
+        children: <Widget>[
+          Positioned(
+            top: -(240 / 4.7),
+            left: screenWidth - (240 / 1.6),
+            child: Opacity(
+              child: Image.asset(
+                ConstsApp.blackPokeball,
+                height: 240,
+                width: 240,
+              ),
+              opacity: 0.1,
+            ),
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: statusWidth,
+                ),
+                AppBarHome(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
